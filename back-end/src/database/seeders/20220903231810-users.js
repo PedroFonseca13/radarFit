c267b89509c663fd402040965199b9aee051af3e
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-09-03 20:33:11 -0300

    feat: implementado seeders, para popular o DB

---
+++ back-end/src/database/seeders/20220903231810-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('products',
+    [
+      {
+        product_name: 'Martelo de quebrar vidro',
+        description: 'Martelo especial, para quebrar todo tipo de vidro.',
+        value: 99.98,
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updated_at: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      {
+        product_name: 'Pano de enxugar gelo',
+        description: 'Pano feito de material exclusivo, destinado ao uso com gelo.',
+        value: 199.90,
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updated_at: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+    ]
+  ),
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
