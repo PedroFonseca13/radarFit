6ca7237623a97643670f6c261390aa7eddc21a87
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-09-05 12:31:22 -0300

    feat: implementado a estrutura do react-router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
-import Header from './components/Header';
-import Products from './components/Section-products'
-import FormAdd from './components/FormAdd/Index'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Post from './pages/Post';
+import Edit from './pages/Edit';
+// import Products from './components/Section-products'
+// import FormAdd from './components/FormAdd/Index'
 
 function App() {
   return (
-    <div className="">
-      <Header />
-      <FormAdd />
-      <Products />
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path='/post' component={ Post } />
+        <Route path='/Edit' component={ Edit } />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
