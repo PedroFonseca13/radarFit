55d74975c4afb0324d56d3d36d13e191b0f4cb4c
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-09-05 14:19:27 -0300

    chore: esqueci que ainda pode nao ter sido avaliado

--- front-end/src/components/FormAdd/Index.js
+++ front-end/src/components/FormAdd/Index.js
@@ -10,12 +10,12 @@ export default function FormAdd() {
         <h3>Produtos</h3>
 
         <FaPlusCircle className="icon-plus" />
-        <form action="http://localhost:3000/products" method="post">
+        {/* <form action="http://localhost:3000/products" method="post">
           <input name="productName" type="text" value="me" placeholder="Product Name" />
           <input name="description" type="text" placeholder="Description" />
           <input name="valor" type="number" placeholder="Valor" />
           <button>Envie meus cumprimentos</button>
-        </form>
+        </form> */}
       </div>
       <hr />
     </div>
